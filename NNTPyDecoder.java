3778c223500fcec4ce83ac68f575935b558943db
Mark Heath <mark@silicontrip.net>
2012-10-09 23:44:22 +1100

    Removed the code to establish connections to the news server. Now requires a NNTPConnection during construction. Does validation of yEnc parts during decode.

--- NNTPyDecoder.java
+++ NNTPyDecoder.java
@@ -8,101 +8,84 @@ public class NNTPyDecoder {
 	private String[] articleList;
 	Part[] parts;
 	YEncDecoder decoder;
-	//NNTPConnection[] nntp;
-	Integer nntpPort;
-	String nntpHost;
+	NNTPConnection nntp;
 	
-	public NNTPyDecoder () throws IOException {
+	public NNTPyDecoder (NNTPConnection n) throws IOException {
 		decoder = new YEncDecoder();
-		Properties fileProperties = new Properties();
+		setNNTPConnection(n);
+		//Properties fileProperties = new Properties();
 		
 		
-		FileInputStream fis = new FileInputStream(new File("nntp.properties"));
-		fileProperties.load (fis);
-		fis.close();
+	//	FileInputStream fis = new FileInputStream(new File("nntp.properties"));
+	//	fileProperties.load (fis);
+	//	fis.close();
 		
-		nntpPort = new Integer(fileProperties.getProperty("NewsServerPort"));
-		nntpHost = fileProperties.getProperty("NewsServerHost");
+	//	nntpPort = new Integer(fileProperties.getProperty("NewsServerPort"));
+	//	nntpHost = fileProperties.getProperty("NewsServerHost");
 		
 	}
 	
-	public NNTPyDecoder (String[] a) throws IOException, YEncException, NNTPNoSuchArticleException, NNTPConnectionResponseException
+	public NNTPyDecoder (NNTPConnection n, String[] a) throws IOException, YEncException, NNTPNoSuchArticleException, NNTPConnectionResponseException
 	{
-		this();
+		this(n);
 		setArticleList(a);
 	}
 	
-	public NNTPyDecoder (String a) throws IOException, YEncException, NNTPNoSuchArticleException, NNTPConnectionResponseException
+	public NNTPyDecoder (NNTPConnection n, String a) throws IOException, YEncException, NNTPNoSuchArticleException, NNTPConnectionResponseException
 	{
-		this();
+		this(n);
 		setArticle(a);
 	}
 	
 	
-	
-	// public void setNNTPConnection (NNTPConnection n) { nntp = n; }
-	
 	/**
-	 * Creates a connection to the News server and issues a Body command on the chosen article.
+	 * Set the NNTP connection to read from.
 	 *
-	 * @param articleName the name of the article to be fetched.
-	 * @return an NNTPConnection to the News Server.
-	 * @throws IOException if a network error occurs.
-	 * @throws NNTPNoSuchArticleException if the chosen article does not exist.
-	 * @throws NNTPConnectionResponseException if news server does not respond with the expected connection response.
+	 * @param n the NNTPConnection.
 	 */
-	protected NNTPConnection nntpFactory(String articleName) throws IOException, NNTPNoSuchArticleException, NNTPConnectionResponseException
-	{
-		NNTPConnection nntp = new NNTPConnection(nntpHost,nntpPort);
-		//nntp.enableDebug();
-		nntp.connect();
-		
-		nntp.bodyArticle(articleName);
-		
-		return nntp;
-	}
+	
+	
+	public void setNNTPConnection (NNTPConnection n) { nntp = n; }
+	
+
 	
 	/**
 	 * Set a single article for decode.
 	 *
 	 * @param article the name of the article for decoding.
-	 * @throws IOException if a network error occurs while trying to read the article from the news server
-	 * @throws NNTPNoSuchArticleException if the article doesn't exist on the news server
-	 * @throws NNTPConnectionResponseException if the news server doesn't respond with the expected connection response
 	 */
 	
-	public void setArticle(String article) throws IOException,  NNTPNoSuchArticleException, NNTPConnectionResponseException
+	public void setArticle(String article) 
 	{
 		parts = new Part[1];
 		
 		parts[0] = new Part();
 		parts[0].file = article;
-		parts[0].nntp = nntpFactory(parts[0].file);
+		//parts[0].nntp = nntpFactory(parts[0].file);
 		
 	}
 	/**
-	 * Sets the article list. Makes connection to the news server.
+	 * Sets the article list for decoding. 
 	 *
-	 * @param articleList a list of the article name to be validated.
+	 * @param articleList a list of article names for decoding.
 	 */
 	
-	public void setArticleList(String[] articleList) throws IOException,  NNTPNoSuchArticleException, NNTPConnectionResponseException
+	public void setArticleList(String[] articleList) 
 	{ 		
 		parts = new Part[articleList.length];
-		
-		//nntp = new NNTPConnection[articleList.length];
-		
-		// get info on all parts
+				
 		for (int i = 0; i < articleList.length; i++) {
 			parts[i] = new Part();
 			parts[i].file = articleList[i];
-			parts[i].nntp = nntpFactory(parts[i].file);
 		}
 		
 		
 	}
 	/**
 	 * Sorts the articles in part order. Validates all the parts exist and are from the same archive.
+	 * Do not run this method if reading for a network stream.  Oh hang on this is for an NNTP server.
+	 * what am I thinking? Do not run this method, unless you don't care about network bandwidth and time.
+	 * Or you are lazy and cannot be bothered sorting the yEnc parts. 
 	 *
 	 * @throws IOException if there was a network error reading from the news server
 	 * @throws YEncException if the articles cannot be decoded. If there are parts missing or if the parts are from different archives.
@@ -113,7 +96,8 @@ public class NNTPyDecoder {
 	{
 		for (int i = 0; i < articleList.length; i++) {
 			decoder.reset();
-			decoder.setInputStream( parts[i].nntp );
+			nntp.bodyArticle(parts[i].file);
+			decoder.setInputStream( nntp );
 			parts[i].name = decoder.getFileName();
 			parts[i].part = decoder.getPartNumber();
 			parts[i].pbegin = decoder.getPartBegin();
@@ -140,9 +124,32 @@ public class NNTPyDecoder {
 	{
 		OutputStream out = null;
 		for (int i = 0; i < parts.length; i++) {
-			// nntp.setArticleName(parts[i].file);
-			parts[i].nntp.reset();
-			decoder.setInputStream(parts[i].nntp);
+			nntp.bodyArticle(parts[i].file);
+			decoder.setInputStream( nntp );
+			
+			parts[i].name = decoder.getFileName();
+			parts[i].part = decoder.getPartNumber();
+			parts[i].pbegin = decoder.getPartBegin();
+			parts[i].pend = decoder.getPartEnd();
+
+			// validate as we go
+			if (i>0) {
+				if (parts[i].part != parts[i-1].part+1)
+					throw new MissingPartsException("Parts are " +
+													"not consecutive (expecting=" +
+													(parts[i-1].part + 1) + "/found=" +
+													parts[i].part + ")");
+				
+				if (!parts[i].name.equals(parts[i-1].name))
+					throw new YEncException("Parts are from " +
+											"different archives");
+				
+				
+				if (parts[i-1].pend+1 != parts[i].pbegin)
+					throw new YEncException("Missing data");
+				
+			}
+			
 			if (out == null) {
 				out = new BufferedOutputStream(
 											   new FileOutputStream(
@@ -163,9 +170,7 @@ public class NNTPyDecoder {
 			decoder.decode();
 			
 			System.out.println("Done.");
-			
-			parts[i].nntp.disconnect();
-			
+						
 		}
 	}		
 	

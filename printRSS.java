8bf289c4a643dfcaa95d089b877c0ff24f1e1126
Mark Heath (WXB035881PR830) <mark@silicontrip.org>
2013-05-15 13:08:00 +1000

    Change the order of the replacement.

--- printRSS.java
+++ printRSS.java
@@ -9,17 +9,17 @@ public class printRSS implements NNTPMatchedArticle {
     public  void processArticle (NNTPConnection n) {
 
         System.out.println("<item>");
-        System.out.println("<title>" + n.getArticleName() .replace("<", "&lt;").replace("&", "&amp;") +"</title>");
-        System.out.println("<link>http://silicontrip.net/~mark/nntpget.php/" + n.getArticleMessageID() .replace("<", "&lt;").replace("&", "&amp;") +"</link>");
+        System.out.println("<title>" + n.getArticleName() .replace("&", "&amp;").replace("<", "&lt;") +"</title>");
+        System.out.println("<link>http://silicontrip.net/~mark/nntpget.php/" + n.getArticleMessageID() .replace("&", "&amp;").replace("<", "&lt;") +"</link>");
         System.out.println("<pubDate>"+ n.getArticleDateAsString() +"</pubDate>");
-        System.out.println("<description>"+ n.getArticleSubject().replace("<", "&lt;").replace("&", "&amp;") +"</description>") ;
+        System.out.println("<description>"+ n.getArticleSubject().replace("&", "&amp;").replace("<", "&lt;") +"</description>") ;
         System.out.println("<content>");
         System.out.println("Bytes: "+ n.getArticleBytes());
         System.out.println("Lines: "+ n.getArticleLines());
-        System.out.println("Path: "+ n.getArticlePath().replace("<", "&lt;").replace("&", "&amp;"));
-        System.out.println("From: "+ n.getArticleFrom().replace("<", "&lt;").replace("&", "&amp;"));
+        System.out.println("Path: "+ n.getArticlePath().replace("&", "&amp;").replace("<", "&lt;") );
+        System.out.println("From: "+ n.getArticleFrom().replace("&", "&amp;").replace("<", "&lt;") );
         System.out.println("Newsgroups: "+ n.getArticleNewsgroups());
-        System.out.println("Message-ID: "+ n.getArticleMessageID().replace("<", "&lt;").replace("&", "&amp;"));
+        System.out.println("Message-ID: "+ n.getArticleMessageID().replace("&", "&amp;").replace("<", "&lt;") );
         System.out.println("Organization: "+ n.getArticleOrganization());
         System.out.println("NNTP-Posting-Host: "+ n.getArticleNNTPPostingHost());
         System.out.println("Xref: "+ n.getArticleXref());

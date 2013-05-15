054b0ebf17da1cdaf6e63f587dead1a88f568d5a
Mark Heath (WXB035881PR830) <mark@silicontrip.org>
2013-05-15 13:34:08 +1000

    Another type of format.

--- printRSS.java
+++ printRSS.java
@@ -12,14 +12,15 @@ public class printRSS implements NNTPMatchedArticle {
         System.out.println("<title>" + n.getArticleSubject() .replace("&", "&amp;").replace("<", "&lt;") +"</title>");
         System.out.println("<link>http://silicontrip.net/~mark/nzb.php/" + n.getArticleMessageID() .replace("&", "&amp;").replace("<", "&lt;") +"</link>");
         System.out.println("<pubDate>"+ n.getArticleDateAsString() +"</pubDate>");
-        System.out.println("<description>"+ n.getArticleSubject().replace("&", "&amp;").replace("<", "&lt;") );
+        System.out.println("<description>");
+        //System.out.println.getArticleSubject().replace("&", "&amp;").replace("<", "&lt;") );
 
       //  System.out.println("<content>");
         System.out.println("Bytes: "+ n.getArticleBytes() );
         System.out.println("Lines: "+ n.getArticleLines() );
         System.out.println("Path: "+ n.getArticlePath().replace("&", "&amp;").replace("<", "&lt;") );
         System.out.println("From: "+ n.getArticleFrom().replace("&", "&amp;").replace("<", "&lt;") );
-        System.out.println("Newsgroups: "+ n.getArticleNewsgroups()+"<br/>");
+        System.out.println("Newsgroups: "+ n.getArticleNewsgroups());
         System.out.println("Message-ID: "+ n.getArticleMessageID().replace("&", "&amp;").replace("<", "&lt;") );
         System.out.println("Organization: "+ n.getArticleOrganization());
         System.out.println("NNTP-Posting-Host: "+ n.getArticleNNTPPostingHost());

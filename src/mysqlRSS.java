0a0be2ba3cc49543b819e532416bbaa8f62ab017
Mark Heath (@utopia) <mjpeg0@silicontrip.net>
2013-11-04 16:24:15 +1100

    mysql RSS article writer.

---
+++ src/mysqlRSS.java
@@ -0,0 +1,48 @@
+import java.sql.*;
+
+
+public class mysqlRSS implements NNTPMatchedArticle {
+
+    protected static final String url = "jdbc:mysql://localhost:3306/newzearch";
+    
+    
+public  void processArticle (NNTPConnection n)  {
+
+   
+    
+    
+    String query = "INSERT INTO NNTPArticle (message_id,creation_time,title,description) VALUES ('" +
+    n.getArticleMessageID() + "', '" +
+    n.getArticleDateAsString() + "', '" +
+    n.getArticleSubject() + "', '" +
+    
+    "Bytes: "+ n.getArticleBytes() +
+    " Lines: "+ n.getArticleLines() +
+    " Path: "+ n.getArticlePath() +
+    " From: "+ n.getArticleFrom() +
+    " Newsgroups: "+ n.getArticleNewsgroups() +
+    " Message-ID: "+ n.getArticleMessageID() +
+    " Organization: "+ n.getArticleOrganization() +
+    " NNTP-Posting-Host: "+ n.getArticleNNTPPostingHost() +
+    " Xref: "+ n.getArticleXref()+
+    "')";
+
+    try {
+    
+        Statement stmt;
+        Connection con = DriverManager.getConnection(url,"nzb", "nznzb");
+        stmt = con.createStatement();
+
+    
+        stmt.executeUpdate(query);
+    
+        con.close();
+
+    } catch (java.sql.SQLException sqe) {
+        
+        System.out.println("SQL Error: " + sqe.getMessage());
+    }
+}
+
+
+}

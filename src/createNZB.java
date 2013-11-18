8203f676f9f8fe0ddbf8461eb8d87765da8a66f0
Mark Heath (@utopia) <mjpeg0@silicontrip.net>
2013-11-18 16:03:07 +1100

    Fixed create NZB

--- src/createNZB.java
+++ src/createNZB.java
@@ -3,7 +3,7 @@
 		NZBfile nzb;
 		String group;
 
-		public createNZB (String g)
+		public createNZB (String g) throws javax.xml.parsers.ParserConfigurationException
 		{
 			nzb= new NZBfile();
 			group = g;
@@ -11,14 +11,14 @@
 		
 		public  void processArticle (NNTPConnection n) {
 
-			String fileSubject = nzb.getFileFromSubject( n.getArticleSubject());			
+			String fileSubject = nzb.getFileNameFromSubject( n.getArticleSubject());
 			String fileSegmentNumber = nzb.getSegmentNumberFromSubject( n.getArticleSubject());			
 
 			synchronized (this) {
 				if (!nzb.hasFileForSubject(fileSubject)) {
 					nzb.addFile(fileSubject,group);
 				}
-				nzb.addSegmentToFile(fileSubject,n.getMessageID(),fileSegmentNumber);
+				nzb.addSegmentToFile(fileSubject,n.getArticleMessageID(),fileSegmentNumber);
 			}
 		}
 	}
